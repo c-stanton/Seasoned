22a328e47312f0a613bae0fd7fce51f3eb91bd31
chloe <chloestanton117@gmail.com>
2026-03-06 22:01:15 +0000

    Added icons

--- Seasoned.Frontend/nuxt.config.ts
+++ Seasoned.Frontend/nuxt.config.ts
@@ -9,6 +9,23 @@ export default defineNuxtConfig({
 
   srcDir: 'app/',
 
+  app: {
+    baseURL: '/',
+    buildAssetsDir: '_nuxt',
+    head: {
+      title: 'Seasoned',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/web-app-manifest-192x192.png' },
+        { rel : 'icon', type: 'image/png', sizes: '512x512', href: '/web-app-manifest-512x512.png' },   
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },    
+        { rel: 'manifest', href: '/site.webmanifest' } 
+      ]
+    }
+  },
+
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
